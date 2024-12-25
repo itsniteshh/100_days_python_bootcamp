8f0b052fc735fde547d4ea519f9dcae7d08128ca
Nitesh Jha <nitesh7258@gmail.com>
2024-12-26 00:34:02 +0530

    Day 2

---
+++ Python Bootcamp - Jose/files.py
@@ -0,0 +1,5 @@
+myfile = open("demo.txt")
+
+print(myfile.read())
+print(myfile.seek(0))
+print(myfile.read())
---
+++ Python Bootcamp - Jose/tuples.py
@@ -0,0 +1,7 @@
+name = ("Nitesh", "jha", "Nitesh")
+print(type(name))
+
+#name[0] = "Nitesh" # This will give an error because tuple is immutable
+
+print(name.count("Nitesh"))
+print(name.index("jha"))
